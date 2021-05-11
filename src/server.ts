2e8e4f406021e26d8e725647424d8c8bf7a26dee
david 🐕 <david.wallin.lewis@gmail.com>
2021-05-10 21:56:14 -0600

    move to deps

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
 import * as cdk from "monocdk";
 import * as ec2 from "monocdk/aws-ec2";
-import * as patterns from "monocdk/aws-ecs-patterns";
 import * as ecs from "monocdk/aws-ecs";
-import * as assets from "monocdk/aws-ecr-assets";
 import * as autoscaling from "monocdk/aws-autoscaling";
-import * as gamelift from "monocdk/aws-gamelift";
-import * as elb from "monocdk/aws-elasticloadbalancingv2";
 import * as secrets from "monocdk/aws-secretsmanager";
-import * as efs from "monocdk/aws-efs";
 
 export class KeukboundServer extends cdk.Stack {
     constructor(parent: cdk.Construct, id: string, props?: cdk.StackProps) {
